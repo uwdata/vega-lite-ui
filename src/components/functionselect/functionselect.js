ef003218129b8f2f194fdab159afdb6aae8cd3b0
Riley Chang <chang.riley.z@gmail.com>
2016-08-17 14:19:34 -0700

    Move filter function out of block scope

--- src/components/functionselect/functionselect.js
+++ src/components/functionselect/functionselect.js
@@ -45,6 +45,14 @@ angular.module('vlui')
           ]
         };
 
+        function cardinalityFilter(timeUnit) {
+          // Convert 'any' channel to '?'.
+          var channel = Pills.isAnyChannel(scope.channelId) ? '?' : scope.channelId;
+          return !timeUnit || // Don't filter undefined.
+            // Remove timeUnits that have cardinality <= 1.
+            Dataset.schema.cardinality({field: Pills.get(scope.channelId).field, channel: channel, timeUnit: timeUnit}, false, true) > 1;
+        }
+
         // timeUnits = T functions - undefined
         var timeUnits = _.pull(_.concat(temporalFunctions.aboveFold, temporalFunctions.belowFold), undefined);
 
@@ -124,13 +132,6 @@ angular.module('vlui')
             scope.func.selected = COUNT;
           } else {
             // TODO: check supported type based on primitive data?
-            function cardinalityFilter(timeUnit) {
-              // Convert 'any' channel to '?'.
-              var channel = Pills.isAnyChannel(scope.channelId) ? '?' : scope.channelId;
-              return !timeUnit || // Don't filter undefined.
-                // Remove timeUnits that have cardinality <= 1.
-                Dataset.schema.cardinality({field: pill.field, channel: channel, timeUnit: timeUnit}, false, true) > 1;
-            }
             if (isT) {
               scope.func.list.aboveFold = temporalFunctions.aboveFold.filter(cardinalityFilter);
               scope.func.list.belowFold = temporalFunctions.belowFold.filter(cardinalityFilter);
